71ba105e300494da5549966009bdedd997542022
Daniel Gärber <daniel.gaerber@student.tugraz.at>
2021-03-19 22:43:15 +0100

    constructed graph and added text

--- lotr_interaction_intensity/connection_graph.py
+++ lotr_interaction_intensity/connection_graph.py
@@ -54,11 +54,6 @@ for i, line in enumerate(full_data["dialog"]):
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'http://www.ageofthering.com/atthemovies/scripts/fellowshipofthering1to4.php'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
 # %%
 book_1_urls = [
     "/atthemovies/scripts/fellowshipofthering1to4.php",
@@ -75,68 +70,251 @@ book_1_urls = [
     "/atthemovies/scripts/fellowshipofthering45to46.php",
     ]
 
+book_2_urls = [
+    "/atthemovies/scripts/thetwotowers1to4.php",
+    "/atthemovies/scripts/thetwotowers5to8.php",
+    "/atthemovies/scripts/thetwotowers9to12.php",
+    "/atthemovies/scripts/thetwotowers13to16.php",
+    "/atthemovies/scripts/thetwotowers17to20.php",
+    "/atthemovies/scripts/thetwotowers21to24.php",
+    "/atthemovies/scripts/thetwotowers25to28.php",
+    "/atthemovies/scripts/thetwotowers29to32.php",
+    "/atthemovies/scripts/thetwotowers33to36.php",
+    "/atthemovies/scripts/thetwotowers37to40.php",
+    "/atthemovies/scripts/thetwotowers41to44.php",
+    "/atthemovies/scripts/thetwotowers45to44.php",
+    "/atthemovies/scripts/thetwotowers49to52.php",
+    "/atthemovies/scripts/thetwotowers53to56.php",
+    "/atthemovies/scripts/thetwotowers57to60.php",
+    "/atthemovies/scripts/thetwotowers61to64.php",
+    "/atthemovies/scripts/thetwotowers65to66.php",
+]
 
-combined_data = pd.DataFrame(columns=["character", "text", "scene"])
-for url in book_1_urls: #[3:4]:
-    print(url)
+book_3_urls = [
+    "/atthemovies/scripts/returnoftheking1to4.php",
+    "/atthemovies/scripts/returnoftheking5to8.php",
+    "/atthemovies/scripts/returnoftheking9to12.php",
+    "/atthemovies/scripts/returnoftheking13to16.php",
+    "/atthemovies/scripts/returnoftheking17to20.php",
+    "/atthemovies/scripts/returnoftheking21to24.php",
+    "/atthemovies/scripts/returnoftheking25to28.php",
+    "/atthemovies/scripts/returnoftheking29to32.php",
+    "/atthemovies/scripts/returnoftheking33to36.php",
+    "/atthemovies/scripts/returnoftheking37to40.php",
+    "/atthemovies/scripts/returnoftheking41to44.php",
+    "/atthemovies/scripts/returnoftheking45to48.php",
+    "atthemovies/scripts/returnoftheking49to52.php",
+    "atthemovies/scripts/returnoftheking53to56.php",
+    "atthemovies/scripts/returnoftheking57to60.php",
+    "atthemovies/scripts/returnoftheking61to64.php",
+    "atthemovies/scripts/returnoftheking65to68.php",
+    "atthemovies/scripts/returnoftheking69to72.php",
+    "atthemovies/scripts/returnoftheking73to76.php",
+]
 
-    page = requests.get("http://www.ageofthering.com/" + url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+movie_urls = [book_1_urls, book_2_urls, book_3_urls]
+movie_names = [
+    "The Fellowship of the Ring",
+    "The Two Towers",
+    "The Return of the King",
+]
 
-    speakers = []
-    texts = []
-    scene_names = []
+combined_data = pd.DataFrame(columns=["character", "text", "scene", "movie"])
 
-    speaker_found = False
-    scene_found = False
-    for i, table in enumerate(soup.find_all("table")):
+for movie_name, urls in zip(movie_names, movie_urls):
 
-        speaker = 0
+    for url in urls: #[3:4]:
+        print(url)
 
-        for ii, row in enumerate(table.find_all("td")):
-            # print(row.text.strip())
-            # print(row.text)
-            # print(re.search(r"Scene\s[0-9]+", row.text))
-            # print(re.search(r"Scene 1", row.text.strip()))
-            # break
-            if re.search(r"Scene\s[0-9]+", row.text):
-                scene_found = True
-                scene_name = row.text.strip().replace("\r\n", "")
-                continue
+        page = requests.get("http://www.ageofthering.com/" + url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-            if not scene_found: continue
-                
-            if not speaker_found and row.get("valign") == "top" and row.get("colspan") == None:
-                # print(row)
-                speaker = row.text.strip().replace(":","")
-                if speaker.isupper():
-                    speakers.append(speaker)
-                    speaker_found = True
-                    scene_names.append(scene_name)
+        speakers = []
+        texts = []
+        scene_names = []
 
-            elif speaker_found:
-                # print(row)
-                text = row.text.strip().replace("\r\n", "\n")
-                text = re.sub(r"\(.*?\)", r"", text)
-                texts.append(text)
-                speaker_found = False
+        speaker_found = False
+        scene_found = False
+        for i, table in enumerate(soup.find_all("table")):
 
-    # print(speakers)
-    # print(texts)
+            speaker = 0
 
-    scene_data = pd.DataFrame({"character":speakers, "text":texts, "scene": scene_names})
-    combined_data = pd.concat((combined_data, scene_data), axis=0)
+            for ii, row in enumerate(table.find_all("td")):
+                if re.search(r"Scene\s[0-9]+", row.text):
+                    scene_found = True
+                    scene_name = row.text.strip().replace("\r\n", "")
+                    continue
 
-combined_data    
+                if not scene_found: continue
+                    
+                if not speaker_found and row.get("valign") == "top" and row.get("colspan") == None:
+                    # print(row)
+                    speaker = row.text.strip().replace(":","")
+                    if speaker.isupper():
+                        speakers.append(speaker)
+                        speaker_found = True
+                        scene_names.append(scene_name)
 
+                elif speaker_found:
+                    # print(row)
+                    text = " ".join(row.text.replace("\r\n", "").split()).strip()
+                    text = re.sub(r"\(.*?\)", r"", text)
+                    texts.append(text)
+                    speaker_found = False
 
+        # print(speakers)
+        # print(texts)
 
+        scene_data = pd.DataFrame({"character":speakers, "text":texts, "scene": scene_names, "movie": movie_name})
+        combined_data = pd.concat((combined_data, scene_data), axis=0)
 
+combined_data
 
+# %%
+# get_missing text
+with open("data/missing_text_37_to_end.txt") as f:
+    raw_missing_text = f.read()
+
+raw_missing_text# %%
+
+raw_missing_text  = re.sub(r"\(.*?\)", r"", raw_missing_text)
+raw_missing_text = raw_missing_text.split("\n")
+raw_missing_text = [x for x in raw_missing_text if x != ""]
+raw_missing_text
+# %%
+speakers = []
+texts = []
+for item in raw_missing_text:
+    try:
+        (speaker, text) = item.split(":", maxsplit=1)
+        speakers.append(speaker.upper())
+        texts.append(text)
+    except:
+        print(item)
 
 # %%
 
-len(speakers) 
+# source: https://www.councilofelrond.com/subject/the-fellowship-of-the-ring-extended-edition/
+# ends at 15
+# starts 16, ends 28
+# starts 29, ends 32
+# starts 33, ends 49
+# starts 50, ends 62
+# starts 63, ends 139
+# starts 140, ends 157
+# starts at 157, ends at 157
+# starts at 158, ends at 162
+scenes = \
+    ["Scene 38 ~ Caras Galadhon"] * 16 + \
+    ["Scene 39 ~ The Mirror of Galadriel"] * 13 + \
+    ["Scene 40 ~ The Fighting Uruk-hai"] * 4 + \
+    ["Scene 41 ~ Farewell to Lorien"] * 17 + \
+    ["Scene 42 ~ The Great River"] * 13 + \
+    ["Scene 43 ~ Parth Galen"] * 77 + \
+    ["Scene 44 ~ The Breaking of the Fellowship"] * 18 + \
+    ["Scene 45 ~ The Departure of Boromir"] * 0 + \
+    ["Scene 46 ~ The Road Goes Ever On"] * 5 
+    
+missing_text = pd.DataFrame({"character":speakers, "text":texts, "scene": scenes, "movie": movie_names[0]})
 
 # %%
+len(speakers)
 
+# %%
+before_missing = combined_data[combined_data["movie"] == movie_names[0]]
+after_missing = combined_data[combined_data["movie"] != movie_names[0]]
+full_data = pd.concat([before_missing, missing_text, after_missing]).reset_index(drop=True)
+full_data.to_csv("./data/lotr_script_extended.csv")
+full_data
+
+
+# %%
+# TODO: fix scene names
+# 
+
+# %%
+# find main characters by counting speaking lines
+main_characters = list(full_data["character"].value_counts().index[:15])
+main_characters# %%
+
+# %%
+# count interactions
+G = nx.Graph()
+
+scene_id_before = -1
+for i in range(len(full_data)-1):
+
+    # check if scene of text is the same
+    if full_data.iloc[i]["scene"] != scene_id_before:
+        scene_id_before = full_data.iloc[i]["scene"]
+        continue
+
+    # scene_id_before = full_data.iloc[i]["scene"]
+    
+    # get characters from the lines
+    c1 = full_data["character"].iloc[i-1]
+    c2 = full_data["character"].iloc[i]
+
+    if c1 not in main_characters or c2 not in main_characters:
+        continue
+
+    sorted_characters = sorted([c1, c2])
+    try:
+        G.edges[sorted_characters]["weight"] += 1
+    except KeyError:
+        G.add_edge(sorted_characters[0], sorted_characters[1], weight=1)
+
+    
+# %%
+# make absolute interaction graph
+plt.figure(figsize=(40,40))
+
+pos = nx.circular_layout(G) 
+# pos = nx.spring_layout(G) 
+
+edges = G.edges()
+colors = [G[u][v]['weight']**0.1 for u,v in edges]
+weights = [G[u][v]['weight']**0.7 if G[u][v]['weight'] > 4 else 0 for u,v in edges]
+
+cmap = matplotlib.cm.get_cmap('YlGn')
+# cmap = matplotlib.cm.get_cmap('ocean')
+nx.draw_networkx(G, pos, width=weights, edge_color=colors, node_color="brown", edge_cmap=cmap, with_labels=False, node_size=700)
+
+labels_pos = {name:[pos_list[0], pos_list[1]-0.04] for name, pos_list in pos.items()}
+nx.draw_networkx_labels(G, labels_pos, font_size=50, font_family="sans-serif", font_color="#000000", font_weight="bold")
+
+ax = plt.gca()
+ax.margins(0.25)
+plt.axis("equal")
+plt.tight_layout()
+plt.savefig("./output/connection_graph_raw.png")
+# %%
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
+img = Image.open("./output/connection_graph_raw.png")
+draw = ImageDraw.Draw(img)
+
+
+font = ImageFont.truetype("./aniron/anirb___.ttf", size=100)
+# draw.text((x, y),"Sample Text",(r,g,b))
+draw.text((700, 0),"  Lord of the Rings\nInteraction Intensity", (0,0,0), font=font,)
+
+font = ImageFont.truetype("./kelt/KELTB___.TTF", size=50)
+description = \
+"""
+Displays the number of times two characters speak after one another, giving an approximation of how much the characters talk to each other. 
+If two characters have less than 4 interactions, no line is drawn. Main source: www.ageofthering.com/atthemovies
+"""
+draw.text((100, 2700), description, (0,0,0), font=font)
+# img.save('sample-out.png')
+
+display(img)
+# %%
+font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 25)
+img = Image.new("RGBA", (200,200), (120,20,20))
+draw = ImageDraw.Draw(img)
+draw.text((0,0), "This is a test", (255,255,0), font=font)
+display(img)
+# %%
